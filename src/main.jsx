d283b413175a3f632e133b3cdc11aff9239fe18c
Richard <richard.palak11@gmail.com>
2025-02-17 18:12:20 +0100

    base defined

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,8 @@ import { HashRouter } from 'react-router-dom';
 import './index.css'
 import App from './App.jsx'
 
+const base = import.meta.env.MODE === 'production' ? '/Food-nutrition-app' : '/';
+
 createRoot(document.getElementById('root')).render(
   <HashRouter basename={base}>
     <App />
